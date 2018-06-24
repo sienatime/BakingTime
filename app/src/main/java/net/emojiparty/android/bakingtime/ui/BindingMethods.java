0a4681dd7cb072662b801900ca487a5e1b9277ed
Siena Aguayo <siena.aguayo@gmail.com>
2018-06-23 19:01:34 -0700

    recipe view stlyin

---
+++ app/src/main/java/net/emojiparty/android/bakingtime/ui/BindingMethods.java
@@ -0,0 +1,23 @@
+package net.emojiparty.android.bakingtime.ui;
+
+import android.databinding.BindingAdapter;
+import android.view.View;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+import net.emojiparty.android.bakingtime.R;
+
+//https://android.jlelse.eu/loading-images-with-data-binding-and-picasso-555dad683fdc
+public class BindingMethods {
+  @BindingAdapter({"imageUrl"})
+  public static void loadImageFromUrl(ImageView view, String url) {
+    if (!url.equals("")) {
+      Picasso.get()
+          .load(url)
+          .placeholder(R.color.colorPrimary)
+          .error(R.color.colorAccent)
+          .into(view);
+    } else {
+      view.setVisibility(View.INVISIBLE);
+    }
+  }
+}
