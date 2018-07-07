e7c0f80abfecf6ad026624606c1b87f8ba9ab21e
Siena Aguayo <siena.aguayo@gmail.com>
2018-07-07 14:14:27 -0700

    tests for next and previous buttons

--- app/src/androidTest/java/net/emojiparty/android/bakingtime/RecipeDetailActivityTest.java
+++ app/src/androidTest/java/net/emojiparty/android/bakingtime/RecipeDetailActivityTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import net.emojiparty.android.bakingtime.data.Recipe;
 import net.emojiparty.android.bakingtime.data.RecipeRepository;
 import net.emojiparty.android.bakingtime.ui.RecipeDetailActivity;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,9 +16,11 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static net.emojiparty.android.bakingtime.ui.RecipeDetailActivity.RECIPE_ID;
+import static org.hamcrest.CoreMatchers.not;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -44,11 +47,62 @@ import static net.emojiparty.android.bakingtime.ui.RecipeDetailActivity.RECIPE_I
         }
       };
 
-  @Test public void clickingRecipeStep_opensStep() {
+  @Before public void assertAcitivtyLoadedWithRecipe() {
     onView(withId(R.id.detail_recipe_name)).check(matches(withText("Nutella Pie")));
+  }
+
+  @Test public void clickingRecipeStep_opensStep() {
     onView(withId(R.id.steps_recycler_view)).perform(
         RecyclerViewActions.actionOnItemAtPosition(0, click()));
     onView(withId(R.id.step_short_description)).check(matches(withText("Recipe Introduction")));
     onView(withId(R.id.step_description)).check(matches(withText("Recipe Introduction")));
+  }
+
+  @Test public void firstStep_hasNextNotPrevious() {
+    onView(withId(R.id.steps_recycler_view)).perform(
+        RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+    onView(withId(R.id.step_short_description)).check(matches(withText("Recipe Introduction")));
+    onView(withId(R.id.step_description)).check(matches(withText("Recipe Introduction")));
+
+    onView(withId(R.id.next)).check(matches(isDisplayed()));
+    onView(withId(R.id.previous)).check(matches(not(isDisplayed())));
+
+    onView(withId(R.id.next)).perform(click());
+    onView(withId(R.id.step_short_description)).check(matches(withText("Starting prep")));
+  }
+
+  @Test public void secondStep_hasNext() {
+    onView(withId(R.id.steps_recycler_view)).perform(
+        RecyclerViewActions.actionOnItemAtPosition(1, click()));
+
+    onView(withId(R.id.step_short_description)).check(matches(withText("Starting prep")));
+    onView(withId(R.id.next)).check(matches(isDisplayed()));
+    onView(withId(R.id.next)).perform(click());
+
+    onView(withId(R.id.step_short_description)).check(matches(withText("Finish filling prep")));
+  }
+
+  @Test public void secondStep_hasPrevious() {
+    onView(withId(R.id.steps_recycler_view)).perform(
+        RecyclerViewActions.actionOnItemAtPosition(1, click()));
+
+    onView(withId(R.id.step_short_description)).check(matches(withText("Starting prep")));
+    onView(withId(R.id.previous)).check(matches(isDisplayed()));
+    onView(withId(R.id.previous)).perform(click());
+
+    onView(withId(R.id.step_short_description)).check(matches(withText("Recipe Introduction")));
+  }
+
+  @Test public void lastStep_hasPreviousNotNext() {
+    onView(withId(R.id.steps_recycler_view)).perform(
+        RecyclerViewActions.actionOnItemAtPosition(2, click()));
+
+    onView(withId(R.id.step_short_description)).check(matches(withText("Finish filling prep")));
+    onView(withId(R.id.next)).check(matches(not(isDisplayed())));
+    onView(withId(R.id.previous)).check(matches(isDisplayed()));
+
+    onView(withId(R.id.previous)).perform(click());
+    onView(withId(R.id.step_short_description)).check(matches(withText("Starting prep")));
   }
 }
--- app/src/androidTest/java/net/emojiparty/android/bakingtime/TestRecipeFactory.java
+++ app/src/androidTest/java/net/emojiparty/android/bakingtime/TestRecipeFactory.java
@@ -35,6 +35,13 @@ public class TestRecipeFactory {
     step0.setVideoURL("https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd974_-intro-creampie/-intro-creampie.mp4");
     step0.setThumbnailURL("");
 
+    Step step1 = new Step();
+    step1.setId(1);
+    step1.setShortDescription("Starting prep");
+    step1.setDescription("1. Preheat the oven to 350\\u00b0F. Butter a 9\\\" deep dish pie pan.");
+    step1.setVideoURL("");
+    step1.setThumbnailURL("");
+
     Step step5 = new Step();
     step5.setId(5);
     step5.setShortDescription("Finish filling prep");
@@ -43,6 +50,7 @@ public class TestRecipeFactory {
     step5.setThumbnailURL("https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffda20_7-add-cream-mix-creampie/7-add-cream-mix-creampie.mp4");
 
     steps.add(step0);
+    steps.add(step1);
     steps.add(step5);
 
     return steps;
